3fe80dede658ff0948120c0d2477c33657903fd2
Informatica <informatica@alumno.local>
2024-02-12 11:57:11 -0600

    Se agregaron las direcciones para conexiones

---
+++ API_ALEJANDROMAX_CONTROL_A/models/alumnos.js
@@ -0,0 +1,31 @@
+const { Schema, model} = require('mongoose');
+
+const AlumnoSchema = Schema ({
+    nomre: {
+        type: String,
+        require: [true, 'El nombre es obligatorio']
+    },
+    correo: {
+        type: String,
+        require: [true, 'El correo es obligatorio']
+    },
+    password: {
+        type: String,
+        require: [true, 'La password es obligatoria']
+    },
+    role: {
+        type: String,
+        enum: ["TEACHER_ROLE","STUDENT_ROLE"],
+        default: "STUDENT_ROLE"
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    office: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = model('Alumno', AlumnoSchema);
